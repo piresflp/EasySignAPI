153ef247d7c5af7d636f184533aa5e8298235b11
Rafael borrozino <rafa.rborroz@gmail.com>
2021-09-15 11:56:07 -0300

    alteraçoes

--- src/controllers/PalavraController.js
+++ src/controllers/PalavraController.js
@@ -40,7 +40,7 @@ module.exports = {
                 .status(404)
                 .json({error: 'Nenhuma palavra foi encontrada.'});
 
-        palavra_desejada.endereco_gif = "http://www.acessibilidadebrasil.org.br/libras_3/" + palavra_desejada.endereco_gif;
+        palavras_desejada.endereco_gif = "http://www.acessibilidadebrasil.org.br/libras_3/" + palavras_desejada.endereco_gif;
 
         return res.json(palavras_desejadas)
     },
