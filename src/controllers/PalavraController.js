28e5f6a29e15337b447e698b4021bf1c5f1baaac
Felipe Pires <flpiresa@gmail.com>
2021-09-15 11:50:04 -0300

    update

--- src/controllers/PalavraController.js
+++ src/controllers/PalavraController.js
@@ -15,6 +15,7 @@ module.exports = {
     },
 
     async consultarPorLetra(req, res){
+        console.log(req.body)
         letra  = req.body.letra_inicial.toLowerCase();
         letras_alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
         'n','o','p','q','r','s','t','u','v','w','x','y','z'];
