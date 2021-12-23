d32ba58f2059a394c6f19a5e5f38b323b3c9ce0b
Vitor Risso <vrisso@lifescan.com>
2021-12-23 19:46:26 -0300

    using use case and removing business rules

--- src/controllers/PalavraController.js
+++ src/controllers/PalavraController.js
@@ -1,45 +1,29 @@
-const Palavra = require('../models/Palavra');
-
+const PalavraUseCase = require('../useCase/PalavraUseCase')
 module.exports = {
     async consultarPorID(req, res){
         const { id } = req.params;
-
-        const palavra_desejada = await Palavra.findByPk(id);
-
-        if(!palavra_desejada)
-            return res
-                .status(404)
-                .json({error: 'Palavra não encontrada.'});
-
-        return res.json(palavra_desejada)
+        
+        try {
+           const palavra_desejada = await PalavraUseCase.procurar(id)  
+           return res.json(palavra_desejada)
+          
+        } catch (error) {
+          return res
+          .status(404)
+          .json({error: 'Palavra não encontrada.'});
+        }
     },
 
     async consultarPorLetra(req, res){
-        console.log(req.body)
-        letra  = req.body.letra_inicial.toLowerCase();
-        letras_alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-        'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        const letra  = req.body.letra_inicial.toLowerCase();
 
-        if(!letras_alfabeto.includes(letra))
-            return res
-                .status(404)
-                .json({error: 'Insira uma letra valida!'});
+        try {
+            const palavras_desejadas = PalavraUseCase.procurarTodos(letra)
+            return res.json(palavras_desejadas)
 
-        const palavras_desejadas = await Palavra.findAll({
-            where: {
-                letra_inicial: letra
-            },
-            attributes: ['palavra', 'significado', 'genero', 'origem', 'endereco_gif', 'exemplo_portugues', 'exemplo_libras'],
-
-        });
-
-        if(palavras_desejadas.length == 0)
-            return res
-                .status(404)
-                .json({error: 'Nenhuma palavra foi encontrada.'});
-
-
-        return res.json(palavras_desejadas)
+        } catch (error) {
+            res.status(404).json({error: error.message})    
+        }
     },
 
     async inserir(req, res){
@@ -52,7 +36,16 @@ module.exports = {
             endereco_gif,
             exemplo_portugues,
             exemplo_libras
-        } = await Palavra.create(req.body);
+        } = req.body;
+        
+        await PalavraUseCase.criar( letra_inicial,
+          palavra,
+          significado,
+          genero,
+          origem,
+          endereco_gif,
+          exemplo_portugues,
+          exemplo_libras);
 
         return res.json(
             letra_inicial,
@@ -63,6 +56,6 @@ module.exports = {
             endereco_gif,
             exemplo_portugues,
             exemplo_libras
-        )
+        );
     }
 }
